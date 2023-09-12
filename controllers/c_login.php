9b1642133c71e11e8771fa520b94c601d6b7470f
Arielito123 <arielmega6@gmail.com>
2023-09-11 23:59:49 -0300

    crud_validacion_login

---
+++ controllers/c_login.php
@@ -0,0 +1,30 @@
+<?php
+// Incluye el archivo de la clase Database (ajusta la ruta según tu estructura de archivos)
+session_start();
+require_once '../model/query.php';
+
+// Crea una instancia de la clase Database y establece la conexión
+$modelSql = new model_sql();
+
+
+// Obtiene los datos del formulario
+$user = $_POST['user'];
+$password = $_POST['password'];
+
+$resul=$modelSql->login($user,$password);
+
+if ($resul!=false) {
+  
+        $name = $row['name'];
+        $_SESSION['name'] = $name;
+
+
+        // Redirige al usuario al dashboard u otra página
+        
+        header("Location: ../views/preisncripcion.php?mensaje=correcto"); // Cambia "dashboard.php" al nombre de tu página de destino
+        exit();
+    } else {
+        echo "Credenciales incorrectas. Por favor, intente de nuevo.";
+    }
+  
+?>
