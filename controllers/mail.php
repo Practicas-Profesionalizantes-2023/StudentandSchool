50f81b17bce6d0a77fbfd170f5a2571e090f9a0e
alejo-nunez <alejonunez45@gmail.com>
2023-10-12 01:02:08 -0300

    cambiar el mensaje del mail

--- controllers/mail.php
+++ controllers/mail.php
@@ -23,24 +23,21 @@ try {
     $mail->addAddress($_POST["email"], 'usuario');
     $mail->isHTML(true);
     $mail->CharSet = 'UTF-8';
-    $mail->Subject = '"¡Preinscripción Exitosa! ¡Bienvenido a institutec!"';
-    $mail->Body = '<p>¡Nos complace informarte que tu preinscripción en institutec ha sido exitosa!</p>' .
-        '<p>Te damos la más cordial bienvenida a nuestra institución educativa.</p>' .
-        '<p>Esperamos que esta sea una experiencia enriquecedora y que te sientas parte de nuestra comunidad</p>' .
-        '<p>desde el primer día. Para completar tu proceso de inscripción, te invitamos a acercarte a</p>' .
-        '<p>la escuela lo antes posible para finalizar los detalles administrativos.</p>' .
-        '<p>Nuestro equipo estará encantado de ayudarte en cualquier consulta que tengas y</p>' .
-        '<p>de brindarte toda la información necesaria para tu inicio exitoso en institutec.</p>' .
-        '<p>No dudes en ponerte en contacto con nosotros si tienes alguna pregunta o necesitas</p>' .
-        '<p>asistencia adicional. Estamos aquí para ayudarte en cada paso del camino.</p>' .
-        '<p>1. Fotocopia del DNI (Documento Nacional de Identidad).</p>' .
-        '<p>2. Partida de nacimiento.</p>' .
-        '<p>3. Analítico de la escuela primaria.</p>' .
-        '<p>4. Analítico de la escuela secundaria.</p>' .
-        '<p>5. Libreta sanitaria actualizada.</p>' .
-        '<p>¡Esperamos verte pronto en nuestra escuela!</p>' .
-        '<p>Atentamente, institutec</p>';
+    $mail->Subject = '"¡Bienvenido a INSTITUTEC!"';
+    $mail->Body = '<p>¡Nos complace informarte que tu preinscripción en INSTITUTEC ha sido exitosa!</p>' .
+         '<p>Para que tu inscripcion sea completada deberas acercarte a la institucion y presentar los siguientes documentos:   </p>' .
+        
+        '<p>Original y copia:</p>'.
+        '<p>1. Fotocopia del DNI (Documento Nacional de Identidad)</p>' .
+        '<p>2. Partida de nacimiento.</p>'. 
+        '<p>3. Analítico de la escuela secundaria. </p>' .
 
+        '<p>Original solo:</p>'.
+        '<p>4. Libreta sanitaria actualizada. La cual podrás pedirla llevando tu libreta de vacunas a alguna salita médica</p>' .
+        
+        '<p>¡Esperamos verte pronto en nuestra escuela!</p>' .
+        '<p>No dudes en ponerte en contacto con nosotros si tienes alguna pregunta o necesitas ayuda.</p>' .
+        '<p>Atentamente, INSTITUTEC!</p>';
     if (!$mail->send()) {
         echo 'Error al enviar el mensaje: ' . $mail->ErrorInfo;
     } else {
